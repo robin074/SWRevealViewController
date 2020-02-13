0630ce5d7b49d2ba5eec138562a438a08542efeb
Apple MacBook Pro <Apple@Apples-MacBook-Pro-2.local>
2020-02-13 10:07:46 +0600

    first commit

---
+++ SwRevealViewController/MenuViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MenuViewController.swift
+//  SwRevealViewController
+//
+//  Created by Apple MacBook Pro on 2/12/20.
+//  Copyright © 2020 Apple MacBook Pro. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SwRevealViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwRevealViewController
+//
+//  Created by Apple MacBook Pro on 2/12/20.
+//  Copyright © 2020 Apple MacBook Pro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
